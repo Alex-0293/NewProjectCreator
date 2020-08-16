92016d7903c6a556917ce9d85c70ec393111b0c9
Alex-on-AB <1928311@tuta.io>
2020-08-17 00:33:16 +0300

    Settings empty.

--- SETTINGS/Settings-empty.ps1
+++ SETTINGS/Settings-empty.ps1
@@ -1,7 +1,7 @@
 # Rename this file to Settings.ps1
 ######################### value replacement #####################
 
-[string] $Global:InitScriptPath      = ""         
+[string] $Global:InitScriptPath      = ""          
 [string] $Global:FinishScript        = ""         
 
 [string] $Global:ScriptAuthor = ""         
@@ -12,21 +12,30 @@
 [string] $Global:NewProjectDescription = ""         
 # https://help.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository#searching-github-by-license-type
 [string] $Global:NewProjectGPL         = ""         
-[string] $Global:ProjectType           = ""          # P- Project, S - Project service
+[string] $Global:ProjectType           = ""          # Select project type [Project/Service/Other]
 
 [bool]   $Global:GitHubPrivateScope    = ""          
 [bool]   $Global:AddRemoteOrigin       = ""          
+[bool]   $Global:InitMermaidDiagram    = ""          
 
 ######################### no replacement ########################
 
-[string] $Global:Component           = "Module: AlexkUtils                   ( https://github.com/Alex-0293/PS-Modules ) `n        Init, finish scripts: GlobalSettings ( https://github.com/Alex-0293/GlobalSettings )"
-[array]  $Global:FolderToRemove      = @("ACL",".git")
+[string] $Global:TemplateScriptFileName = "ScriptTemplate.ps1" 
+[string] $Global:Component              = "Module: AlexkUtils                   ( https://github.com/Alex-0293/PS-Modules ) `n        Init, finish scripts: GlobalSettings ( https://github.com/Alex-0293/GlobalSettings )"
+[array]  $Global:FolderToRemove         = @("ACL", ".git")
+
+[string] $Global:MermaidDiagram         = @"
+#################################  Mermaid diagram  #################################
+```mermaid
+
+```
+"@
 
 $Global:ScriptParams = [PSCustomObject]@{
     Author         = $Global:ScriptAuthor 
     Ver            = $Global:ScriptVer
     Lang           = $Global:ScriptLang
-    Component      = ""#$Global:Component 
+    Component      = $Global:Component 
     Date           = (Get-Date -Format $GlobalDateFormat)
     InitScriptPath = $InitScriptPath
     FinishScript   = $FinishScript
